a638af10d600b8efc2b0027e87a7db9e20f94fe8
Tito Yslas <roberto.yslas@gmail.com>
2018-04-20 15:11:29 -0600

    refactored HeroesComponent into two pieces - HeroesComponent & HeroDetailsComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 // components
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
