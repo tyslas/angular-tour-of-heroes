dc1830097be5c81e412687872c6ae235f66709ef
Tito Yslas <roberto.yslas@gmail.com>
2018-04-24 12:04:32 -0600

    added full routing capability to HeroDetailComponent - extract id route parameter within the ngOnInit lifecycle hook, added getHero() method to the HeroService, added a back button to the HeroDetailComponent

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -14,6 +14,12 @@ export class HeroService {
 
   constructor(private messageService: MessageService) { }
 
+  getHero(id: number): Observable<Hero> {
+    // TODO: seand the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetching hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
+
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
