171dde5038bc639344a3b59ed0952172c3b7f593
Tito Yslas <roberto.yslas@gmail.com>
2018-04-27 13:51:35 -0600

    added DELETE functionality for removing heroes from the list

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,6 +11,7 @@ import { MessageService } from './message.service';
 // mock data
 // import { HEROES } from './mock-heroes';
 
+// http header
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
@@ -75,6 +76,17 @@ export class HeroService {
     return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
       tap((her: Hero) => this.log(`added hero w/ id=${hero.id}`)),
       catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
+  /** DELETE: delete the hero from the server */
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
     );
   }
 }
