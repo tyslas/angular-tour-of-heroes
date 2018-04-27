5b5cfc177ad792261df5899995bdf1f9273416e4
Tito Yslas <roberto.yslas@gmail.com>
2018-04-27 11:33:02 -0600

    enabled http services, simulated data server, get heroes with HttpClient module, Http methods return a single value, HttpClient.get() returns response data, error handling implemented for data calls, tap into an observable, GET here by id, update hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,28 +1,73 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+import { catchError, map, tap } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 // obj template
 import { Hero } from './hero';
 // message service
 import { MessageService } from './message.service';
 // mock data
-import { HEROES } from './mock-heroes';
+// import { HEROES } from './mock-heroes';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable()
 export class HeroService {
+  private heroesUrl = 'api/heroes'; // URL to web api
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
-  getHero(id: number): Observable<Hero> {
-    // TODO: seand the message _after_ fetching the hero
-    this.messageService.add(`HeroService: fetching hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+  private log(message: string) {
+    this.messageService.add('HeroService: ' + message);
   }
 
+  /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+  /** GET hero by id - will 404 if not found */
+  getHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetching hero id=${id}`)),
+      catchError(this.handleError<Hero>(`fetched hero id=${id}`))
+    );
+  }
+  /** GET heroes from server */
   getHeroes(): Observable<Hero[]> {
-    // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl)
+    .pipe(
+      tap(heroes => this.log('fetched heroes')),
+      catchError(this.handleError('getHeroes', []))
+    );
+  }
+
+  /** PUT: update the hero on the server */
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
   }
 }
